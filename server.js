8b6bd1920ed7cb4455263c8ff944f48f0950d3e0
amgp1012 <alvaro.m.guevara@gmail.com>
2022-10-08 15:02:32 -0700

    database connection to heroku env

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-concave-26396',
-    port : 5432,
-    user : 'Alvaro',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
